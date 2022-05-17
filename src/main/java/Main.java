2da5744a54d8cbd16bb5e8c0c6682f97fecfcdd5
Dmytro Getmanskyy <getmanskyy.dmitry@gmail.com>
2022-05-17 16:20:06 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 38;
-        Factorial factorial = new Factorial();
-
-        System.out.printf("Factorial of %d is %d\n", number, factorial.factorial(number));
+        System.out.println("Ok");
     }
 }
